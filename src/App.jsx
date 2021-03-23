f8b0e59b978df902e452d18b9426a65abe7ccd75
Nathan Donnelly <email@nathandonnelly.com>
2021-03-23 16:19:13 -0400

    Functional App Template Updates with Bootstrap, Data & SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ class VSTDB extends React.Component {
     super(props);
     this.state = {
       name: "VSTDB",
-      description: "a database for virtual instruments and effects",
+      description: "A Database for Virtual Instruments and Effects",
       category: "N/A",
       id: "6bc0c736-5724-4eeb-8922-80fa087f976f",
       icon: "./icons/diztro/vstdb.png",
@@ -18,15 +18,15 @@ class VSTDB extends React.Component {
   }
   render(){
     return (
-      <div className="VSTDB">
-        <header className="site-header">
-          <h1 className="site-name">{PROJECT_NAME.toLowerCase()}</h1>
+      <div className="wrapper">
+        <header>
+          <h1>{PROJECT_NAME.toLowerCase()}</h1>
         </header>
-        <main className="application">
-          <div className="current-vst">
-            <h2 className="current-vst-name text-center">{this.state.name}</h2>
+        <main>
+          <div className="container-fluid">
+            <h2>{this.state.name}</h2>
             <img src={this.state.icon} alt={this.state.name + " - " + PROJECT_NAME} />
-            <table className="vst-data">
+            <table className="table">
               <tr><th>Item</th><th>Result</th></tr>
               <tr><td>Name</td><td>{this.state.name}</td></tr>
               <tr><td>Description</td><td>{this.state.description}</td></tr>
@@ -37,15 +37,19 @@ class VSTDB extends React.Component {
             </table>
           </div>
         </main>
+        <footer>
+          <div className="row no-gutters text-center">
+            <div className="col">
+              <p>Made with <i className="fa fa-heart footer-heart" /> by <a href="https://nathandonnelly.com">Nathan Donnelly</a> at <a href="https://diztro.com">Diztro</a> for <a href="https://image-line.com">FL Studio</a> users</p>
+            </div>
+          </div>
+        </footer>
       </div>
     );
   }
 }
 
 class App extends React.Component{
-  constructor(props){
-    super(props)
-  }
   render(){
     return (
       <VSTDB />
