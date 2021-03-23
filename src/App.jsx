8e71bf2f3f7c733b1ba294f1e4553709a1a700c1
Nathan Donnelly <email@nathandonnelly.com>
2021-03-23 14:05:07 -0400

    Initialized create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+{/*VSTDB*/}
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: 0
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="test">Hello World</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
