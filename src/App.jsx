f4d1b01d8c3443d420a4943792a4fee98c90c83e
Nathan Donnelly <email@nathandonnelly.com>
2021-03-23 15:22:23 -0400

    Working on data template.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,54 @@
 import React from 'react';
 
+const PROJECT_NAME = "VSTDB"
 
-class App extends React.Component {
+class VSTDB extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      value: 0
+      name: "VSTDB",
+      description: "a database for virtual instruments and effects",
+      category: "N/A",
+      id: "6bc0c736-5724-4eeb-8922-80fa087f976f",
+      icon: "./icons/diztro/vstdb.png",
+      vendor: "Diztro",
+      developer: "bc79981a-5bf0-4242-8122-90997b18da1e",
+      url: "https://vstdb.org"
     }
   }
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="test">Hello World</h1>
+      <div className="VSTDB">
+        <header className="site-header">
+          <h1 className="site-name">{PROJECT_NAME.toLowerCase()}</h1>
         </header>
+        <main className="application">
+          <div className="current-vst">
+            <h2 className="current-vst-name text-center">{this.state.name}</h2>
+            <img src={this.state.icon} alt={this.state.name + " - " + PROJECT_NAME} />
+            <table className="vst-data">
+              <tr><th>Item</th><th>Result</th></tr>
+              <tr><td>Name</td><td>{this.state.name}</td></tr>
+              <tr><td>Description</td><td>{this.state.description}</td></tr>
+              <tr><td>Category</td><td>{this.state.category}</td></tr>
+              <tr><td>Vendor</td><td>{this.state.vendor}</td></tr>
+              <tr><td>URL</td><td>{this.state.url}</td></tr>
+              <tr><td>ID</td><td>{this.state.id}</td></tr>
+            </table>
+          </div>
+        </main>
       </div>
+    );
+  }
+}
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+      <VSTDB />
     );
   }
 }
---
+++ src/Data.js
@@ -0,0 +1,22 @@
+export const VST_PLUGINS = [
+    {
+        name: "Serum",
+        description: "Advanced Wavetable Synthesizer",
+        category: "Synth special",
+        id: "fb794cce-1cf6-4367-b31c-0c7dc1ee4276",
+        icon: "./icons/development/xfer-records/serum.png",
+        vendor: "Xfer Records",
+        developer: "e3ff48d1-63d3-4c5a-916a-1bd706aa4aff",
+        url: "https://xferrecords.com"
+    },
+    {
+        name: "Serum FX",
+        description: "Modular FX Rack",
+        category: "Misc",
+        id: "88a832ca-85dc-4552-9551-246e84c7b49c",
+        icon: "./icons/development/xfer-records/serum-fx.png",
+        vendor: "Xfer Records",
+        developer: "e3ff48d1-63d3-4c5a-916a-1bd706aa4aff",
+        url: "https://xferrecords.com"
+    }
+]
