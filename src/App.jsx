9a1a79aba525a937aa410f4f9fb2de4fae12e034
Nathan Donnelly <email@nathandonnelly.com>
2021-03-25 23:32:36 -0400

    Updated with working functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,153 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { PLUGINS } from './Plugins.js';
 
-const PROJECT_NAME = "VSTDB"
-let GLOBAL_COUNT = 0;
+const PROJECT_NAME = "VSTdb"
+let todaysDate = new Date();
 
-class VSTDB extends React.Component {
+
+const Header = (props) => {
+  return(
+    <header className="header">
+      <div className="row no-gutters">
+        <nav className="navbar navbar-expand-lg">
+          <div className="col"><a href="/"><h1>{PROJECT_NAME}</h1></a></div>
+          <div className="col">
+            <button onClick={props.decreaseStateValue}>-</button>
+            <button onclick={props.randomStateValue}>R</button>
+            <button onClick={props.increaseStateValue}>+</button>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+
+const Footer = (props) => {
+  return (
+    <footer>
+      <div className="row no-gutters">
+        <div className="col">
+          <p className="text-center">Made with <i className="fa fa-heart" /> by <a href="https://nathandonnelly.com">Nathan Donnelly</a> at <a href="https://diztro.com">Diztro</a> for <a href="https://image-line.com">FL Studio</a> users</p>
+          <p className="text-center"><i className="fa fa-copyright"/> Copyright {props.copyrightDate}</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.defaultProps = { copyrightDate: 2020 }
+Footer.propTypes = { copyrightDate: PropTypes.number.isRequired }
+
+
+const Display = (props) => {
+  return(
+    <main>
+      <div className="container-fluid">
+        <div className="row no-gutters">
+          <span className="vstdb-component-icon-container">
+            {props.renderImages}
+          </span>
+        </div>
+        <div className="row no-gutters">
+          <table className="table mt-5">
+            <tr><th>Data Point</th><th>Data</th></tr>
+            <tr><td>Alphabetical Order of Plugin by Vendor</td><td>{props.stateValueIndexNumber}</td></tr>
+            <tr><td>Vendor Name</td><td>{props.stateValueVendorName}</td></tr>
+            <tr><td>Vendor ID</td><td>{props.stateValueVendorID}</td></tr>
+            <tr><td>Vendor URL</td><td>{props.stateValueVendorURL}</td></tr>
+            <tr><td>Plugin Name</td><td>{props.stateValuePluginName}</td></tr>
+            <tr><td>Plugin Description</td><td>{props.stateValuePluginDesc}</td></tr>
+            <tr><td>Plugin Category</td><td>{props.stateValuePluginCat}</td></tr>
+            <tr><td>Plugin ID</td><td>{props.stateValuePluginID}</td></tr>
+          </table>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      vendorName: PLUGINS[GLOBAL_COUNT].vendorName,
-      vendorID:   PLUGINS[GLOBAL_COUNT].vendorID,
-      vendorURL:  PLUGINS[GLOBAL_COUNT].vendorURL,
-      pluginName: PLUGINS[GLOBAL_COUNT].pluginName,
-      pluginDesc: PLUGINS[GLOBAL_COUNT].pluginDesc,
-      pluginCat:  PLUGINS[GLOBAL_COUNT].pluginCat,
-      pluginID:   PLUGINS[GLOBAL_COUNT].pluginID,
-      pluginIcon: PLUGINS[GLOBAL_COUNT].pluginIcon
-    }
-    this.nextPlugin = this.nextPlugin.bind(this);
-    this.previousPlugin = this.previousPlugin.bind(this);
+      stateValue: 0,
+      vendorName: "Vendor Name",
+      vendorID:   "Vendor ID",
+      vendorURL:  "Vendor URL",
+      pluginName: "Plugin Name",
+      pluginDesc: "Plugin Description",
+      pluginCat:  "Plugin Category",
+      pluginIcon:  "Plugin Icon",
+      pluginID:   "Plugin ID"
+    };
+    this.increaseValue = this.increaseValue.bind(this);
+    this.decreaseValue = this.decreaseValue.bind(this);
+    this.randomValue = this.randomValue.bind(this);
   }
 
-  nextPlugin = () => {
-    return GLOBAL_COUNT = GLOBAL_COUNT + 1;
+  increaseValue(){
+    return this.setState(state => ({
+      stateValue: this.state.stateValue + 1
+    }));
+  };
+
+  decreaseValue(){
+    if (this.state.stateValue === 0 ){
+      return this.setState(state => ({
+        stateValue: 0
+      }));
+    } else return this.setState(state => ({
+      stateValue: this.state.stateValue - 1
+    }));
   }
-  previousPlugin = () => {
-    return GLOBAL_COUNT = GLOBAL_COUNT - 1;
+
+  randomValue(inputData){
+    return this.setState(state => ({
+      stateValue: Math.floor(Math.random * inputData.length)
+    }));
   }
-  
+
   render(){
+    let vendorName  =   PLUGINS[this.state.stateValue].vendorName;
+    let vendorID    =   PLUGINS[this.state.stateValue].vendorID;
+    let vendorURL   =   PLUGINS[this.state.stateValue].vendorURL;
+    let pluginName  =   PLUGINS[this.state.stateValue].pluginName;
+    let pluginDesc  =   PLUGINS[this.state.stateValue].pluginDesc;
+    let pluginCat   =   PLUGINS[this.state.stateValue].pluginCat;
+    let pluginIcon  =   PLUGINS[this.state.stateValue].pluginIcon;
+    let pluginID    =   PLUGINS[this.state.stateValue].pluginID;
+
+    let renderPlugins = PLUGINS.map(function(plugin){
+      return <img src={plugin.pluginIcon}
+                  alt={plugin.pluginName}
+                  className="vstdb-component-icon"
+                  id={plugin.pluginName + "-VSTdbComponent"} draggable="false" />
+                  
+    })
+
     return (
       <div className="wrapper">
-        <header>
-          <div className="row no-gutters">
-            <div className="col">
-              <h1>{PROJECT_NAME.toLowerCase()}</h1>
-            </div>
-            <div className="col">
-              <button onClick={this.previousPlugin}>-</button>
-              <button onClick={this.nextPlugin}>+</button>
-            </div>
-          </div>
-        </header>
-        <main>
-          <div className="container-fluid">
-            <div className="row no-gutters text-center">
-              <div className="col">
-                <h2>{this.state.pluginName}</h2>
-                <p>{this.state.pluginDesc}</p>
-                <img src={this.state.pluginIcon} alt={this.state.pluginName + " - " + PROJECT_NAME} draggable="false" />
-              </div>
-            </div>
-            <div className="row no-gutters">
-              <table className="table">
-                <tr><th>Item</th><th>Result</th></tr>
-                <tr><td>Vendor</td><td>{this.state.vendorName}</td></tr>
-                <tr><td>Vendor URL</td><td>{this.state.vendorURL}</td></tr>
-                <tr><td>Plugin Name</td><td>{this.state.pluginName}</td></tr>
-                <tr><td>Description</td><td>{this.state.pluginDesc}</td></tr>
-                <tr><td>Category</td><td>{this.state.pluginCat}</td></tr>
-                <tr><td>ID</td><td>{this.state.pluginID}</td></tr>
-              </table>
-            </div>
-          </div>
-        </main>
-        <footer>
-          <div className="row no-gutters text-center">
-            <div className="col">
-              <p>Made with <i className="fa fa-heart" /> by <a href="https://nathandonnelly.com" id="nathandonnelly">Nathan Donnelly</a> at <a href="https://diztro.com" id="diztro">Diztro</a> for <a href="https://image-line.com" id="flstudio">FL Studio</a> users</p>
-            </div>
-          </div>
-        </footer>
-      </div>
-    );
-  }
-}
+        <Header
+          decreaseStateValue={this.decreaseValue}
+          randomStateValue={this.randomValue}
+          increaseStateValue={this.increaseValue} />
 
-class App extends React.Component{
-  render(){
-    return (
-      <VSTDB />
+        <Display
+          renderImages={renderPlugins} 
+          stateValueIndexNumber={this.state.stateValue}
+          stateValueVendorName={vendorName}
+          stateValueVendorID={vendorID} 
+          stateValueVendorURL={vendorURL} 
+          stateValuePluginName={pluginName}
+          stateValuePluginDesc={pluginDesc}
+          stateValuePluginCat={pluginCat}
+          stateValuePluginID={pluginID} />
+
+        <Footer copyrightDate={ todaysDate.getFullYear() }/>
+      </div>
     );
   }
 }
